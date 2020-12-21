6c13ffcc6ae47f8ee85df9cc1a5fa3890c8e7df8
malixi <692628969@qq.com>
2020-12-22 03:11:24 +0800

    spring cloud 全家桶--网关服务成功

---
+++ my-spring-admin/src/test/java/com/malixi/spring/myspringadmin/MySpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malixi.spring.myspringadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
