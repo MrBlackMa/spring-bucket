0b3635470705f50d6422ae3ad3b2d46dcca2fb29
malixi <692628969@qq.com>
2020-12-25 18:22:35 +0800

    单机情况下 使用的数据源 可以不用排除DataSourceAutoConfigration

--- malixi-springboot/src/main/java/com/malixi/springboot/malixispringboot/MalixiSpringbootApplication.java
+++ malixi-springboot/src/main/java/com/malixi/springboot/malixispringboot/MalixiSpringbootApplication.java
@@ -3,8 +3,8 @@ package com.malixi.springboot.malixispringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication
 public class MalixiSpringbootApplication {
 
 	public static void main(String[] args) {
