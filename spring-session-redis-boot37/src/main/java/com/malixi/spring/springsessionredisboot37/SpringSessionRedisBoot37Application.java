52d9db0bcc1c1176931900d0e2eda7ce5138ab73
malixi <692628969@qq.com>
2020-12-30 20:43:15 +0800

    spring 集群 session 共享

---
+++ spring-session-redis-boot37/src/main/java/com/malixi/spring/springsessionredisboot37/SpringSessionRedisBoot37Application.java
@@ -0,0 +1,13 @@
+package com.malixi.spring.springsessionredisboot37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionRedisBoot37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionRedisBoot37Application.class, args);
+	}
+
+}
---
+++ spring-session-redis-boot37/src/main/java/com/malixi/spring/springsessionredisboot37/config/HttpSessionConfig.java
@@ -0,0 +1,21 @@
+package com.malixi.spring.springsessionredisboot37.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+
+@Configuration
+public class HttpSessionConfig {
+
+    /**
+     *  解决redis集群环境没有开启Keyspace notifications导致的
+     *
+     *  Error creating bean with name 'enableRedisKeyspaceNotificationsInitializer'
+     *  defined in class path resource
+     *
+     * */
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+}
