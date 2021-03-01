125a0e993899af15208f4cfcff76f0ce14c191c7
malixi <692628969@qq.com>
2021-03-01 20:37:12 +0800

    sentinel限流器

---
+++ ali-sentinel/src/main/java/com/malixi/spring/alisentinel/AliSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.malixi.spring.alisentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliSentinelApplication.class, args);
+	}
+
+}
---
+++ ali-sentinel/src/test/java/com/malixi/spring/alisentinel/AliSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malixi.spring.alisentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
